d374d65b90757b45be7bf64b86cb853a3e526903
burgosz <burgosz@88f314ff-4cd6-91d3-9818-c83448fab66c>
2011-03-19 00:41:56 +0000

    Mapban átírtam az objectet AElementre, nekem rendesen fordul úgy is, nézzétek meg.

--- AGate.java
+++ AGate.java
@@ -6,7 +6,7 @@ public abstract class AGate extends AElement
 {
 
 //      protected PinIn[] inputs;                   //ezt "tömbösítettem" mondván h nem csak 1 input van :)
-        protected Map<Integer,Object> inputs;
+        protected Map<Integer,PinIn> inputs;
         //elements.put(ID, new OR(inputnum));
 	protected int inputnum;
 	protected PinOut output;
