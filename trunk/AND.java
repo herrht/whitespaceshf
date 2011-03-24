4bc2c0d28dfe8b0cf2fa03abc9d44e934e12c631
herrht <herrht@88f314ff-4cd6-91d3-9818-c83448fab66c>
2011-03-24 22:54:54 +0000

    GetID kiíratásban this legyen.

--- trunk/AND.java
+++ trunk/AND.java
@@ -24,7 +24,7 @@ public class AND extends AGate {
     }
 
     public int GetID() {
-        System.out.println("AND | GetID() | CALL\n");       //kiírat
+        System.out.println(this + " | GetID() | CALL\n");       //kiírat
         return this.ID;         //visszaadja az elem sorszámát
     }
 
