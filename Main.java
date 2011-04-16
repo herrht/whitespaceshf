17ff6646b1abab0a951ca0513fc915c9c79cc7ed
szakaly.tibi <szakaly.tibi@88f314ff-4cd6-91d3-9818-c83448fab66c>
2011-04-16 18:46:13 +0000

    beolvasás megvalósítása fajlbóóóól, talán a müködőképes

--- Main.java
+++ Main.java
@@ -25,46 +25,80 @@ public class Main
         progi.tester.Test9();
         
 
+//        try
+//        {
+//            BufferedReader in = new BufferedReader(new FileReader("in.txt"));   //beolvasandó fájl megadása
+//            String beolv = "";      //parancs amibe be fogunk olvasni
+//            char ch;                // karakterenként fogunk olvasni
+//            int i = 0;              // param tömb indexelésére
+//            int param[]= new int[4];// Command konstruktorának adjuk meg, hogy milyen paraméterekket kapott a beolvasott parancs
+//
+//            boolean flag = false; //volt-e már szoköz a bemeneten a parancs után
+//
+//            ch = (char)in.read();   // egy karakter beolvasása
+//            while(true)             // EOF-ig olvas ami majd exception lesz és leáll az olvasás -> catch
+//            {
+//                while(ch != 13)     // ha entert kaptunk, akkor utána uj parancsot kezdunk
+//                {
+//                    if(ch == ' ')   // ha szoközt kaptunk
+//                    {
+//
+//                        param[i] = (int)in.read();  // beolvassuk a parancs i. paraméterét a paramba
+//                        ++i;                        // param következő indexe
+//                        flag = true;                // volt már szokőz az olvasás során => a parancsot(beolv) már nem kell módosítani
+//                    }
+//                    else if(flag == true)           //nem szoköz, nem enter és ha még nem volt szóköz
+//                    {
+//                        beolv = beolv + ch;         // beolvasott parancshoz hozzáfűzi az uj karaktert
+//                    }
+//                    ch = (char)in.read();           // következő karakter olvasása
+//                }
+//                if(ch == 13)                        // ha enert olvastun be..
+//                {
+//                    Command cmd = new Command(progi.proj, beolv, param);    // meghívjuk a parancsot a megkapott paraméterekkel
+//                    beolv = "";                     // uj parancsot kezdünk az uj sorban
+//                    i = 0;                          // param tomb indexelését előről kezdjuk
+//                }
+//            }
+//        }
+//        catch(EOFException e)                       // EOFException elkapása
+//        {
+//            System.out.println("A fájl beolvasása végetért!");
+//        }
+
         try
         {
             BufferedReader in = new BufferedReader(new FileReader("in.txt"));   //beolvasandó fájl megadása
-            String beolv = "";      //parancs amibe be fogunk olvasni
-            char ch;                // karakterenként fogunk olvasni
-            int i = 0;              // param tömb indexelésére
-            int param[]= new int[4];// Command konstruktorának adjuk meg, hogy milyen paraméterekket kapott a beolvasott parancs
-
-            boolean flag = false; //volt-e már szoköz a bemeneten a parancs után
-
-            ch = (char)in.read();   // egy karakter beolvasása
-            while(true)             // EOF-ig olvas ami majd exception lesz és leáll az olvasás -> catch
+            String sor = new String();
+            String parancs;
+            while( (sor = in.readLine() ) != null)
             {
-                while(ch != 13)     // ha entert kaptunk, akkor utána uj parancsot kezdunk
+                String darabolt[] = sor.split(" ");
+                int param[] = {-1, -1, -1, -1};
+                int count = darabolt.length;
+                if(count == 2)
                 {
-                    if(ch == ' ')   // ha szoközt kaptunk
-                    {
-                        
-                        param[i] = (int)in.read();  // beolvassuk a parancs i. paraméterét a paramba
-                        ++i;                        // param következő indexe
-                        flag = true;                // volt már szokőz az olvasás során => a parancsot(beolv) már nem kell módosítani
-                    }
-                    else if(flag == true)           //nem szoköz, nem enter és ha még nem volt szóköz
-                    {
-                        beolv = beolv + ch;         // beolvasott parancshoz hozzáfűzi az uj karaktert
-                    }
-                    ch = (char)in.read();           // következő karakter olvasása
+                    int tmp = Integer.valueOf(darabolt[1]);
+                    param[0] = tmp;
                 }
-                if(ch == 13)                        // ha enert olvastun be..
+                if(count == 4)
                 {
-                    Command cmd = new Command(progi.proj, beolv, param);    // meghívjuk a parancsot a megkapott paraméterekkel
-                    beolv = "";                     // uj parancsot kezdünk az uj sorban
-                    i = 0;                          // param tomb indexelését előről kezdjuk
+                    for(int i = 1; i < 4; ++i)
+                    {
+                        int tmp = Integer.valueOf(darabolt[i]);
+                        param[i] = tmp;
+                    }
                 }
+                Command cmd = new Command(progi.proj ,darabolt[0], param);
             }
+            in.close();
         }
+        
         catch(EOFException e)                       // EOFException elkapása
         {
             System.out.println("A fájl beolvasása végetért!");
         }
+
 
     }   // End of MAIN
 }
